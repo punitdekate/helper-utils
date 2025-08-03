5f82e4407167a12e13d612cc6da2e683ef10c3c2
punitdekate <82708970+punitdekate@users.noreply.github.com>
2025-08-04 00:48:57 +0530

    fixed import

--- app.js
+++ app.js
@@ -1,11 +1,11 @@
-const responses = require('./responses');
+const responses = require("./responses.js");
 
 module.exports = {
-    SuccessResponse: responses.SuccessResponse,
-    FailureResponse: responses.FailureResponse,
-    BadRequest: responses.BadRequest,
-    Unauthorized: responses.Unauthorized,
-    Forbidden: responses.Forbidden,
-    ResourceNotFound: responses.ResourceNotFound,
-    InternalServerError: responses.InternalServerError
+  SuccessResponse: responses.SuccessResponse,
+  FailureResponse: responses.FailureResponse,
+  BadRequest: responses.BadRequest,
+  Unauthorized: responses.Unauthorized,
+  Forbidden: responses.Forbidden,
+  ResourceNotFound: responses.ResourceNotFound,
+  InternalServerError: responses.InternalServerError,
 };
