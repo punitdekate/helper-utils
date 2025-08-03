1c0092b8879604ced300cef59eb71e9d4b67643d
punitdekate <82708970+punitdekate@users.noreply.github.com>
2025-08-04 01:02:55 +0530

    fixed

--- app.js
+++ app.js
@@ -1,11 +1,5 @@
-const responses = require("./utils/Responses.js");
+const responses = require("./utils/Responses");
 
 module.exports = {
-  SuccessResponse: responses.SuccessResponse,
-  FailureResponse: responses.FailureResponse,
-  BadRequest: responses.BadRequest,
-  Unauthorized: responses.Unauthorized,
-  Forbidden: responses.Forbidden,
-  ResourceNotFound: responses.ResourceNotFound,
-  InternalServerError: responses.InternalServerError,
+  ...responses,
 };
