a1cc67d7b46b1228c0854703d0183adb84d5def5
punitdekate <82708970+punitdekate@users.noreply.github.com>
2025-08-07 21:50:39 +0530

    Added logger

--- utils/schemas/LogSchema.js
+++ utils/schemas/LogSchema.js
@@ -1,6 +1,6 @@
 "use strict";
 const mongoose = require("mongoose");
-
+let LogModel;
 const LogSchema = new mongoose.Schema({
     requestId: { type: String, required: true, index: true },
     userId: { type: String, default: null },
@@ -13,12 +13,12 @@ const LogSchema = new mongoose.Schema({
     timestamp: { type: Date, default: Date.now }
 });
 
-
 // Function to get/create a model for a given table name
-const LogModel = (logTable) => {
+const AssignLogTableName = logTable => {
     if (mongoose.models[logTable]) {
-      return mongoose.model(logTable);
+        LogModel = mongoose.model(logTable);
     }
-    return mongoose.model(logTable, LogSchema);
-  };
-module.exports = { LogModel };
+    LogModel = mongoose.model(logTable, LogSchema);
+};
+
+module.exports = { LogModel, AssignLogTableName };
