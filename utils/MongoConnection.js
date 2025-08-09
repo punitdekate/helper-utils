25e3aa4e11a9639982e30f85a19b4f1e9d66793d
punitdekate <82708970+punitdekate@users.noreply.github.com>
2025-08-09 18:51:00 +0530

    Upadted the connection

--- utils/MongoConnection.js
+++ utils/MongoConnection.js
@@ -5,12 +5,14 @@ const MAX_RETRY = 3;
 async function mongoConnect({ connectionString, retry = 0 }) {
     try {
         logger.info(`Connecting with database ${connectionString} on retry ${retry}`);
-        mongoose
-            .connect(connectionString, {
-                useNewUrlParser: true,
-                useUnifiedTopology: true
-            })
-            .then(() => logger.info("Connected database successfully."));
+        if (!connectionString) {
+            throw new Error("Connection string is required");
+        }
+        const userDB = await mongoose.createConnection(connectionString, {
+            useNewUrlParser: true,
+            useUnifiedTopology: true
+        });
+        return userDB;
     } catch (error) {
         logger.error(`Error in connectToDB : ${error.message}`);
         if (retry < MAX_RETRY) {
