d2ab11c99309bbc75ca2d079c14221dd7e45fa85
punitdekate <82708970+punitdekate@users.noreply.github.com>
2025-08-09 22:01:48 +0530

    Added logger

--- utils/MongoConnection.js
+++ utils/MongoConnection.js
@@ -4,7 +4,6 @@ const MAX_RETRY = 3;
 
 async function mongoConnect({ connectionString, retry = 0 }) {
     try {
-        logger.info(`Connecting with database ${connectionString} on retry ${retry}`);
         if (!connectionString) {
             throw new Error("Connection string is required");
         }
@@ -14,7 +13,7 @@ async function mongoConnect({ connectionString, retry = 0 }) {
         });
         return userDB;
     } catch (error) {
-        logger.error(`Error in connectToDB : ${error.message}`);
+        console.error(`Error in connectToDB : ${error.message}`);
         if (retry < MAX_RETRY) {
             mongoConnect({ connectionString, retry: ++retry });
         } else {
